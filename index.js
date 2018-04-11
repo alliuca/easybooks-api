42127c280390c63456307bd2dc2ee41355b252e7
Luca Allievi <alliuka@gmail.com>
2018-04-11 12:28:43 +0200

    add other routes, including pdf generation. booyah!

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const express = require('express');
+const PDFDocument = require('pdfkit');
+const invoiceTemplate = require('./files/templates/invoice.js');
 const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
@@ -17,12 +20,40 @@ app.get('/api/invoices', (req, res) => {
       return res.status(500).send({ message: `${err}` });
 
     var output = [];
-    files.map(file => {
+    files.filter(file => file.split('.').pop() !== 'pdf').map(file => {
       const data = fs.readFileSync(`${invoicesDirPath}/${file}`, 'utf8');
       output.push(JSON.parse(data));
     });
     res.status(200).send(output);
   });
+});
+
+app.get('/api/invoices/:number', (req, res) => {
+  const invoiceToGet = req.params.number;
+  const invoiceToGetPath = `${invoicesDirPath}/invoice-${invoiceToGet}.json`;
+
+  if (!fs.existsSync(invoiceToGetPath))
+    return res.status(200).send({ message: `Invoice #${invoiceToGet} not found` });
+
+  const data = fs.readFileSync(invoiceToGetPath, 'utf8');
+  res.status(200).send(JSON.parse(data));
+});
+
+app.get('/api/invoices/:number/pdf', (req, res) => {
+  const invoiceToGet = req.params.number;
+  const invoiceToGetPath = `${invoicesDirPath}/invoice-${invoiceToGet}.json`;
+  const invoicePdfPath = `${invoicesDirPath}/invoice-${invoiceToGet}.pdf`;
+
+  if (!fs.existsSync(invoiceToGetPath))
+    return res.status(200).send({ message: `Invoice #${invoiceToGet} not found` });
+  const data = fs.readFileSync(invoiceToGetPath, 'utf8');
+
+  const doc = new PDFDocument();
+
+  doc.pipe(fs.createWriteStream(invoicePdfPath));
+  invoiceTemplate(doc, JSON.parse(data));
+
+  res.status(200).send(`${invoicePdfPath} created`);
 });
 
 app.post('/api/invoices/:number', (req, res) => {
@@ -42,8 +73,20 @@ app.post('/api/invoices/:number', (req, res) => {
       if (err)
         return res.status(500).send({ message: `${err}` });
 
-      res.status(200).send(`Invoice #${newInvoiceNumber} has ben saved correctly`);
+      res.status(200).send(`Invoice #${newInvoiceNumber} has been saved correctly`);
     });
+  });
+});
+
+app.delete('/api/invoices/:number', (req, res) => {
+  const invoiceToDelete = req.params.number;
+  const invoiceToDeletePath = `${invoicesDirPath}/invoice-${invoiceToDelete}.json`;
+
+  fs.unlink(invoiceToDeletePath, (err) => {
+    if (err)
+      return res.status(500).send({ message: `${err}` });
+
+    res.status(200).send(`Invoice #${invoiceToDelete} has been deleted`);
   });
 });
 
